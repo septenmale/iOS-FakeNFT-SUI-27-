47e3fdc9ea016e34b815801b2b32ca17b0923508
Fennec-meow <krisbojchenko@gmail.com>
2025-10-10 18:03:06 +0300

    Import assets from Figma to be used across the project.

--- iOS-FakeNFT-Extended/DesignSystem/Colors.swift
+++ iOS-FakeNFT-Extended/DesignSystem/Colors.swift
@@ -25,16 +25,17 @@ extension UIColor {
         )
     }
 
-    // Ниже приведены примеры цветов, настоящие цвета надо взять из фигмы
-
-    // Primary Colors
-    static let primary = UIColor(red: 0 / 255, green: 122 / 255, blue: 255 / 255, alpha: 1.0)
-
-    // Secondary Colors
-    static let secondary = UIColor(red: 255 / 255, green: 193 / 255, blue: 7 / 255, alpha: 1.0)
-
-    // Background Colors
-    static let background = UIColor.white
+    // Universal Colors
+    enum Universal {
+        static let gray = UIColor(red: 98 / 255, green: 92 / 255, blue: 92 / 255, alpha: 1.0)
+        static let red = UIColor(red: 245 / 255, green: 107 / 255, blue: 108 / 255, alpha: 1.0)
+        static let background = UIColor(red: 26 / 255, green: 27 / 255, blue: 34 / 255, alpha: 0.5)
+        static let green = UIColor(red: 28 / 255, green: 159 / 255, blue: 0 / 255, alpha: 1.0)
+        static let blue = UIColor(red: 10 / 255, green: 132 / 255, blue: 255 / 255, alpha: 1.0)
+        static let black = UIColor(red: 26 / 255, green: 27 / 255, blue: 34 / 255, alpha: 1.0)
+        static let white = UIColor(red: 255 / 255, green: 255 / 255, blue: 255 / 255, alpha: 1.0)
+        static let yellow = UIColor(red: 254 / 255, green: 239 / 255, blue: 13 / 255, alpha: 1.0)
+    }
 
     // Text Colors
     static let textPrimary = UIColor.black
@@ -42,8 +43,8 @@ extension UIColor {
     static let textOnPrimary = UIColor.white
     static let textOnSecondary = UIColor.black
 
-    private static let yaBlackLight = UIColor(hexString: "1A1B22")
-    private static let yaBlackDark = UIColor.white
+    private static let yaBlackLight = UIColor(hexString: "#1A1B22")
+    private static let yaBlackDark = UIColor(hexString: "#FFFFFF")
     private static let yaLightGrayLight = UIColor(hexString: "#F7F7F8")
     private static let yaLightGrayDark = UIColor(hexString: "#2C2C2E")
 
