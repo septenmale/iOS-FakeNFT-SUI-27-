fc1c6bef1e04f0dcf5559010230717bb103f1a85
Owi Lover <lazygabriel@yandex.ru>
2025-10-11 19:24:40 +0300

    Created TabBar, folders for each View and changed Localizable to updated version

---
+++ iOS-FakeNFT-Extended/UI/Profile/ProfileView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Owi Lover on 10/11/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello, ProfileView!")
+    }
+}
--- iOS-FakeNFT-Extended/UI/TabBarView.swift
+++ iOS-FakeNFT-Extended/UI/TabBarView.swift
@@ -1,13 +1,46 @@
 import SwiftUI
 
 struct TabBarView: View {
+    
+    init() {
+        UITabBar.appearance().unselectedItemTintColor = .black
+    }
+    
     var body: some View {
         TabView {
-            TestCatalogView()
+            ProfileView()
+                .tabItem {
+                    Label(
+                        NSLocalizedString("Tab.profile", comment: ""),
+                        image: "profile"
+                    )
+                }
+                .backgroundStyle(.background)
+            
+            CatalogView()
                 .tabItem {
                     Label(
                         NSLocalizedString("Tab.catalog", comment: ""),
-                        systemImage: "square.stack.3d.up.fill"
+                        image: "catalog"
+                    )
+                }
+                .backgroundStyle(.background)
+                .foregroundStyle(.black)
+            
+            CartView()
+                .tabItem {
+                    Label(
+                        NSLocalizedString("Tab.cart", comment: ""),
+                        image: "basket"
+                    )
+                }
+                .backgroundStyle(.background)
+            
+            StatsView()
+                .tabItem {
+                    Label(
+                        NSLocalizedString("Tab.stats", comment: ""),
+                        image: "statistics"
                     )
                 }
                 .backgroundStyle(.background)
