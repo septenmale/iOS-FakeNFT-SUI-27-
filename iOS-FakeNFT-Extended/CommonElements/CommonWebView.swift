2a8cfd3cfc033215c7c8d52caf86a5f96ae27625
Owi Lover <lazygabriel@yandex.ru>
2025-10-27 14:02:18 +0300

    feature: added CommonWebView

---
+++ iOS-FakeNFT-Extended/CommonElements/CommonWebView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct CommonWebView: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    let url: String
+    
+    var body: some View {
+        WebView.init(url: URL(string: url))
+            .edgesIgnoringSafeArea(.bottom)
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                    }
+                    .foregroundStyle(.yaBlack)
+                }
+            }
+            .background(Color.yaWhite)
+    }
+}
